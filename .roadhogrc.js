9abff0be66372e45ed62fb1ba4748fb69a6770d8
zhengqingxin <362965772@qq.com>
2017-08-29 23:04:51 +0800

    fe init

---
+++ .roadhogrc.js
@@ -0,0 +1,37 @@
+export default {
+  entry: 'www/static/src/index.js',
+  publicPath:'/static/dist/',
+  outputPath:'www/static/dist',
+  "env": {
+      "development": {
+        "extraBabelPlugins": [
+          "dva-hmr",
+          "transform-runtime",
+  		    ["import", { "libraryName": "antd", "style": true }],
+          ["module-resolver", {
+            "root": ["./"],
+            "alias": {
+              "components":"www/static//src/components",
+              "utils":"www/static/src/utils",
+              "services":"www/static//src/services"
+            }
+          }]
+        ]
+      },
+      "production": {
+        "extraBabelPlugins": [
+          "transform-runtime",
+  		    ["import", { "libraryName": "antd", "style": true}],
+          ["module-resolver", {
+            "root": ["./"],
+            "alias": {
+              "components":"www/static//src/components",
+              "utils":"www/static//src/utils",
+              "services":"www/static//src/services"
+            }
+          }]
+        ],
+      }
+  }
+
+}
--- www/static/src/components/Layout/Header/index.js
+++ www/static/src/components/Layout/Header/index.js
@@ -16,9 +16,9 @@ const MainHeader = () => {
         defaultSelectedKeys={['2']}
         style={{ lineHeight: '64px' }}
       >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">菜单1</Menu.Item>
+        <Menu.Item key="2">菜单2</Menu.Item>
+        <Menu.Item key="3">菜单3</Menu.Item>
       </Menu>
     </Header>
   )
