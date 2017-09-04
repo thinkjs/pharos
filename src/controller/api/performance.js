cd32b3d78346bf0c5f12f0c8ea2159a877745266
lizheming <secretlzm007@gmail.com>
2017-09-04 23:32:01 +0800

    增加 ip 和 ua 获取和分析

--- src/controller/api/performance.js
+++ src/controller/api/performance.js
@@ -1,5 +1,12 @@
+const path = require('path');
 const crypto = require('crypto');
-const BaseRest = require('../rest.js');
+const detector = require('detector');
+const BaseRest = require('../rest');
+const IPService = require('../../service/ip');
+const ipServiceInstance = new IPService(path.join(
+  think.ROOT_PATH, 'www/ip.dat'
+));
+
 module.exports = class extends BaseRest {
   /** 获取访问用户 id */
   get visitUser() {
@@ -15,25 +22,62 @@ module.exports = class extends BaseRest {
 
   /** 根据 UA 获取信息 */
   get userUA() {
-    return {};
+    const ua = detector.parse(this.userAgent);
+
+    return {
+      browser_engine: ua.engine.name,
+      browser_name: ua.browser.name,
+      browser_version: ua.broswer.version,
+      device_brand: ua.device.name,
+      // device_model: ua.device,
+      // device_type:,
+      device_pixel: this.get('screen'),
+      os: ua.os.name,
+      os_version: ua.os.version
+    };
+  }
+
+  /** 根据 IP 获取位置信息 */
+  get userIP() {
+    const [
+      location_country,
+      location_province,
+      location_city,
+      location_isp
+    ] = ipServiceInstance.find(this.ip);
+    return {
+      location_ip: this.ip,
+      location_country,
+      location_province,
+      location_city,
+      location_isp
+    };
   }
 
   async getAction() {
     const visitUser = this.visitUser;
-    const userInfo = this.userUA;
-    userInfo.screen = this.get('screen');
-    userInfo.ip = this.ip;
-    userInfo.loc = this.getIpLoc(this.ip);
+    const ua = this.userUA;
+    const ip = this.userIP;
 
-    const user = await this.modelInstance.where({
+    const user = {
+      idvisitor: visitUser,
+      last_action_time: think.datetime(),
+      ...ua,
+      ...ip
+    };
+
+    user.visit_url = {};
+    user.performance = {};
+
+    const userInfo = await this.modelInstance.where({
       idvisitor: visitUser
     }).find();
-    userInfo.last_action_time = think.datetime();
-    if (!think.isEmpty(user)) {
-      userInfo.first_action_time = userInfo.last_action_time;
-      await this.modelInstance.add(userInfo);
+
+    if (!think.isEmpty(userInfo)) {
+      user.first_action_time = user.last_action_time;
+      await this.modelInstance.add(user);
     } else {
-      await this.modelInstance.where({id: user.id}).update(userInfo);
+      await this.modelInstance.where({id: userInfo.id}).update(user);
     }
 
     return this.success();
---
+++ src/service/ip.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const dns = require('dns');
+
+module.exports = class {
+  constructor(file) {
+    this.dataBuffer = undefined;
+    console.log(file);
+    this.load(file);
+  }
+
+  load(file) {
+    if (this.dataBuffer) {
+      return this.dataBuffer;
+    }
+
+    this.dataBuffer = this.loadBinaryData(file);
+    return this.dataBuffer;
+  }
+
+  loadBinaryData(filepath) {
+    const fd = fs.openSync(filepath, 'r');
+    const chunkSize = 102400;
+    const chunks = [];
+    let chunkBuffer;
+
+    let readLength = 0;
+    let bufferLength = 0;
+
+    while (true) {
+      chunkBuffer = Buffer.alloc(chunkSize);
+      readLength = fs.readSync(fd, chunkBuffer, 0, chunkSize, bufferLength);
+      bufferLength += readLength;
+      chunks.push(chunkBuffer);
+      if (readLength < chunkSize) break;
+    }
+    fs.closeSync(fd);
+
+    return Buffer.concat(chunks);
+  };
+
+  find(ip) {
+    if (!this.dataBuffer) {
+      return [];
+    }
+
+    const ipArray = ip.trim().split('.');
+    const ip2long = ip => Buffer.from(ip.trim().split('.')).readInt32BE(0);
+    const ipInt = ip2long(ip);
+
+    const offset = this.dataBuffer.readInt32BE(0);
+    const indexBuffer = this.dataBuffer.slice(4, offset - 4 + 4);
+    const tmpOffset = ipArray[0] * 4;
+    const maxCompLen = offset - 1028;
+    let indexOffset = -1;
+    let indexLength = -1;
+    let start = indexBuffer.slice(tmpOffset, tmpOffset + 4).readInt32LE(0);
+
+    for (start = start * 8 + 1024; start < maxCompLen; start += 8) {
+      if (indexBuffer.slice(start, start + 4).readInt32BE(0) >= ipInt) {
+        indexOffset = ((indexBuffer[start + 6] << 16) + (indexBuffer[start + 5] << 8) + indexBuffer[start + 4]);
+        indexLength = indexBuffer[start + 7];
+        break;
+      }
+    }
+    if (indexOffset === -1 || indexLength === -1) {
+      return [];
+    } else {
+      return this.dataBuffer.slice(offset + indexOffset - 1024, offset + indexOffset - 1024 + indexLength).toString('utf-8').split('\t');
+    }
+  };
+
+  ipFind(name) {
+    return new Promise(resolve => {
+      dns.resolve4(name, (err, addresses) => {
+        if (err) {
+          resolve(this.find(name));
+        } else {
+          resolve(this.find(addresses.shift()));
+        }
+      });
+    });
+  };
+};
