1d96ff76fc8054f41fc8fe0185123be296f984c1
lizheming <secretlzm007@gmail.com>
2017-09-16 11:02:42 +0800

    performance api add url and per data

--- src/logic/api/performance.js
+++ src/logic/api/performance.js
@@ -1,14 +1,18 @@
 module.exports = class extends think.Logic {
   getAction() {
-    // this.rules = {
-    //   screen: {
-    //     required: true,
-    //     regexp: /\d+x\d+/
-    //   },
-    //   info: {
-    //     required: true,
-    //     json: true
-    //   }
-    // };
+    this.rules = {
+      title: {
+        required: true,
+        string: true
+      },
+      screen: {
+        required: true,
+        regexp: /\d+x\d+/
+      },
+      info: {
+        required: true,
+        json: true
+      }
+    };
   }
 };
