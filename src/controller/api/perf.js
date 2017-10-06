527d0f811fc943f75cc8c2b023376e1f1cadfb15
lizheming <secretlzm007@gmail.com>
2017-10-06 15:35:08 +0800

    增加获取 perf 各时段次数接口

--- src/controller/api/perf.js
+++ src/controller/api/perf.js
@@ -31,11 +31,11 @@ module.exports = class extends BaseRest {
 
     const sql = `SELECT 
       ${PERFS.map(perf => `AVG(${perf}) AS ${perf}`).join()} 
-    FROM ph_performance
+    FROM ${this.modelPerf.tablePrefix}_performance
     WHERE 
       site_id=${site_id} AND 
       create_time>="${start_time}" AND 
-      create_time<="${end_time}"
+      create_time<"${end_time}"
       ${site_page_id ? ` AND site_page_id=${site_page_id}` : ''}
     `;
     data = await this.modelPerf.query(sql);
@@ -55,5 +55,119 @@ module.exports = class extends BaseRest {
     }));
     await this.modelArch.addMany(data);
     return this.success(data);
+  }
+
+  /** 某 site_id 某 site_page_id 某 perf 因素各时段个数 */
+  async getperfDistributionTime() {
+    const {site_id, site_page_id, start_time, end_time} = this.get();
+    const perf_name = this.get('perf_name').split(/\s*,\s*/);
+    const where = {site_id, start_time, end_time};
+    if (site_page_id) { where.site_page_id = site_page_id }
+    let distributionArea = [
+      [0, '0.1s以内'],
+      [100, '0.1s-0.25s'],
+      [250, '0.25-0.5s'],
+      [500, '0.5-1s'],
+      [1000, '1-2s'],
+      [2000, '2-3s'],
+      [3000, '3-4s'],
+      [4000, '4-5s'],
+      [5000, '5-6s'],
+      [6000, '6-7s']
+    ];
+    if (perf_name.length > 1) {
+      distributionArea = distributionArea.concat([
+        [7000, '7-8s'],
+        [8000, '8-9s'],
+        [9000, '9-10s'],
+        [10000, '10-11s'],
+        [11000, '11-12s'],
+        [12000, '12-13s'],
+        [13000, '13-14s'],
+        [14000, '14-15s'],
+        [15000, '15-16s'],
+        [Number.MAX_SAFE_INTEGER, '16s以上']
+      ]);
+    }
+    distributionArea = new Map(distributionArea);
+
+    let data = await this.modelArch.where(
+      Object.assign({}, where, {
+        index_name: ['IN', perf_name.map(
+          name => `${name}DistributionTime`
+        )]
+      })
+    ).field('index_name as name, index_value as area, index_count as count').select();
+
+    if (data.length === perf_name.length * distributionArea.length) {
+      const obj = {};
+      for (const item of data) {
+        obj[item.name.slice(0, -16)][item.area] = item.count;
+      }
+
+      let result = {};
+      for (const name of perf_name) {
+        for (const area of distributionArea.keys()) {
+          result[name].push({
+            label: distributionArea.get(area),
+            count: data[name] && data[name][area] ? data[name][area] : 0
+          });
+        }
+      }
+      if (perf_name.length === 1) {
+        result = result[perf_name[0]];
+      }
+      return this.success(result);
+    }
+
+    let resultObj = {};
+    for (const name of perf_name) {
+      const distributionInterval = `ELT(
+      INTERVAL(${name}, ${Array.from(distributionArea.keys())}), 
+      ${Array.from(distributionArea.values()).map(label => `'${label}'`)}
+    )`;
+      const sql = `SELECT 
+      site_id,
+      site_page_id,
+      ${distributionInterval} AS level,
+      COUNT(id) AS cnt
+    FROM
+      ${this.modelPerf.tablePrefix}performance
+    WHERE
+      site_id = ${site_id} AND
+      create_time >= "${start_time}" AND create_time < "${end_time}"
+      ${site_page_id ? ` AND site_page_id=${site_page_id}` : ''}
+    GROUP BY
+      ${distributionInterval}
+    `;
+      data = await this.modelPerf.query(sql);
+      data = new Map(data.map(item => [item.level, item]));
+      const result = new Map();
+      for (const areaName of distributionArea.keys()) {
+        const area = distributionArea.get(areaName);
+        result.set(areaName, {
+          label: area,
+          count: data.has(area) ? data.get(area).cnt : 0
+        });
+      }
+
+      resultObj[name] = Array.from(result.values());
+      await this.modelArch.addMany(
+        Array.from(result.keys()).map(area => ({
+          site_id,
+          site_page_id,
+          index_name: `${perf_name}DistributionTime`,
+          index_value: area,
+          index_count: result.get(area).count,
+          start_time,
+          end_time,
+          archive_time: think.datetime()
+        }))
+      );
+    }
+    if (perf_name.length === 1) {
+      resultObj = resultObj[perf_name[0]];
+    }
+    return this.success(resultObj);
   }
 };
