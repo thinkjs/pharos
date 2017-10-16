747ebd3597cbc277c7ff050d74433e521e62ecf3
lizheming <secretlzm007@gmail.com>
2017-10-16 21:52:52 +0800

    增加 emptyController

--- src/controller/base.js
+++ src/controller/base.js
@@ -4,4 +4,8 @@ module.exports = class extends think.Controller {
     const userInfo = await this.session('userInfo') || {};
     this.assign({userInfo: JSON.stringify(userInfo, null, '\t')});
   }
+
+  async __call() {
+    return this.display('index_index');
+  }
 };
