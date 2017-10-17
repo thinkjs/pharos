061ee38b153e3343b46d9df2ad03912f7962a141
lizheming <secretlzm007@gmail.com>
2017-10-17 10:04:53 +0800

    /api/user 增加搜索支持

--- src/controller/api/user.js
+++ src/controller/api/user.js
@@ -1,13 +1,20 @@
 const BaseRest = require('../rest.js');
 module.exports = class extends BaseRest {
   async getAction() {
-    const {page, pagesize} = this.get();
+    const {page, pagesize, keyword} = this.get();
 
     let data;
     if (this.id) {
       const pk = await this.modelInstance.pk;
       data = await this.modelInstance.where({[pk]: this.id}).find();
       return this.success(data);
+    } else if (keyword) {
+      this.modelInstance = this.modelInstance.where({
+        name: ['like', '%' + keyword + '%'],
+        email: ['like', '%' + keyword + '%'],
+        display_name: ['like', '%' + keyword + '%'],
+        _logic: 'OR'
+      });
     }
     data = await this.modelInstance.page([page, pagesize]).countSelect();
     return this.success(data);
