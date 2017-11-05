2568f4c0d5fd7e3e3bb8b3955db1323175d2f4c4
lizheming <secretlzm007@gmail.com>
2017-11-05 13:51:47 +0800

    修改 /api/user 各接口权限

--- src/logic/api/base.js
+++ src/logic/api/base.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = class extends think.Logic {
   async __before() {
     const userInfo = await this.session('userInfo') || {};
@@ -5,6 +6,26 @@ module.exports = class extends think.Logic {
       this.fail('USER_NOT_LOGIN');
     }
     this.userInfo = userInfo;
+  }
+
+  /**
+   * get resource
+   * @return {String} [resource name]
+   */
+  get resource() {
+    const filename = this.ctx.controller;
+    return filename.split('/').pop();
+  }
+  get id() {
+    const id = this.get('id');
+    if (id && (think.isString(id) || think.isNumber(id))) {
+      return parseInt(id);
+    }
+    const last = this.ctx.path.split('/').pop();
+    if (last !== this.resource && /\d+/.test(last)) {
+      return parseInt(last);
+    }
+    return undefined;
   }
 
   async siteUserCheck() {
