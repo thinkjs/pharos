8892d957895739b5344e667ea102605d73087085
lizheming <secretlzm007@gmail.com>
2017-11-08 21:40:35 +0800

    增加版本更新提示

--- src/controller/api/system.js
+++ src/controller/api/system.js
@@ -1,12 +1,13 @@
 // const fs = require('fs');
 // const path = require('path');
-// const semver = require('semver');
-// const request = require('request');
+const semver = require('semver');
 // const cluster = require('cluster');
 // const {exec} = require('child_process');
+const rq = require('request-promise-native');
 const pkg = require('../../../package.json');
 
 const Base = require('./base');
+const lastestUrl = 'https://api.github.com/repos/thinkjs/pharos/releases/latest';
 module.exports = class extends Base {
   constructor(...args) {
     super(...args);
@@ -14,6 +15,24 @@ module.exports = class extends Base {
   }
 
   async getAction() {
+    const update = {
+      need: false,
+      download: '',
+      text: ''
+    };
+    try {
+      const res = await rq(lastestUrl);
+      const lastest = JSON.parse(res.body);
+      if (!lastest.tag_name || !semver.gt(lastest.tag_name, pkg.version)) {
+        throw new Error('Current version is new version now!');
+      }
+
+      update.need = lastest.tag_name;
+      update.download = lastest.assets[0].browser_download_user;
+      update.text = lastest.body;
+    } catch (e) {
+      think.logger.error(e);
+    }
     const mysql = await this.modelInstance.query('SELECT VERSION() as version');
     const data = {
       nodeVersion: process.versions.node,
@@ -21,7 +40,8 @@ module.exports = class extends Base {
       platform: process.platform,
       thinkjsVersion: think.version,
       pharosVersion: pkg.version,
-      mysqlVersion: mysql[0].version
+      mysqlVersion: mysql[0].version,
+      update
     };
     return this.success(data);
   }
