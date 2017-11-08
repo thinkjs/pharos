265260b4ec1c167b051eb18de5eb91b55f7dea16
lizheming <secretlzm007@gmail.com>
2017-11-08 21:46:53 +0800

    add doc

--- src/logic/api/system.js
+++ src/logic/api/system.js
@@ -1,4 +1,11 @@
 const Base = require('./base');
 module.exports = class extends Base {
+  /**
+   * @api {GET} /system 查看系统信息
+   * @apiGroup System
+   * @apiVersion  0.0.1
+   */  
+  getAction() {
 
+  }
 };
