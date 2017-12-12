932671d040efd632163333e91b5f79742ca510cd
lizheming <secretlzm007@gmail.com>
2017-12-12 21:22:34 +0800

    change referrer check and set status value force in user/post

--- src/controller/api/base.js
+++ src/controller/api/base.js
@@ -2,13 +2,6 @@ const BaseRest = require('../rest');
 
 module.exports = class extends BaseRest {
   async __before() {
-    if (!this.isCli && !this.isGet) {
-      const referrer = this.referrer(true);
-      const {site_url} = await this.model('options').getOptions();
-      if (referrer.indexOf(site_url) !== 0) {
-        return this.fail();
-      }
-    }
     // session
     const userInfo = await this.session('userInfo') || {};
     this.userInfo = userInfo;
