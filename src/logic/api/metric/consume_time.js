34b0b52ec1caf9b53664491a58e59eb4bee60633
lizheming <secretlzm007@gmail.com>
2017-12-17 19:39:46 +0800

    change day consume time end_time

--- src/logic/api/metric/consume_time.js
+++ src/logic/api/metric/consume_time.js
@@ -4,17 +4,17 @@ module.exports = class extends Base {
    * @api {GET} /metric/consume_time 获取某段时间所有指标的平均值
    * @apiGroup Performance
    * @apiVersion  0.0.1
-   * 
+   *
    * @apiParam  {Int}     site_id 网站ID
    * @apiParam  {Int}     site_page_id 网页ID
    * @apiParam  {String}  start_time  起始时间
    * @apiParam  {String}  end_time  终止时间
-   */    
+   */
   /**
    * @api {GET} /metric/consume_time?type=interval 性能耗时按照耗时区间分布
    * @apiGroup Performance
    * @apiVersion  0.0.1
-   * 
+   *
    * @apiParam  {Int}     site_id 网站ID
    * @apiParam  {Int}     site_page_id 网页ID
    * @apiParam  {String}  start_time  起始时间
@@ -24,24 +24,28 @@ module.exports = class extends Base {
    * @api {GET} /metric/consume_time?type=hour  性能耗时按照小时分布
    * @apiGroup Performance
    * @apiVersion  0.0.1
-   * 
+   *
    * @apiParam  {Int}     site_id 网站ID
    * @apiParam  {Int}     site_page_id 网页ID
    * @apiParam  {String}  start_time  起始时间
    * @apiParam  {String}  end_time  终止时间
-   */   
+   */
   /**
    * @api {GET} /metric/consume_time?type=day 性能耗时按照日期分布
    * @apiGroup Performance
    * @apiVersion  0.0.1
-   * 
+   *
    * @apiParam  {Int}     site_id 网站ID
    * @apiParam  {Int}     site_page_id 网页ID
    * @apiParam  {String}  start_time  起始时间
    * @apiParam  {String}  end_time  终止时间
-   */     
+   */
 
   getAction() {
+    const ONE_DAY = 24 * 3600000;
+    const ONE_MONTH = ONE_DAY * 30;
+    const INTERVAL = this.get('type') === 'day' ? ONE_MONTH : ONE_DAY;
+
     this.rules = {
       site_id: {
         required: true,
@@ -54,7 +58,7 @@ module.exports = class extends Base {
         default: think.datetime(Date.now(), 'YYYY-MM-DD')
       },
       end_time: {
-        default: think.datetime(Date.now() + 24 * 3600000, 'YYYY-MM-DD')
+        default: think.datetime(Date.now() + INTERVAL, 'YYYY-MM-DD')
       },
       type: {
         in: ['interval', 'hour', 'day']
