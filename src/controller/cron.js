98b2c07d85523ab5cf36fd8c0d430fcd31b7d992
lizheming <secretlzm007@gmail.com>
2018-07-01 14:59:22 +0800

    fix cron table prefix

--- src/controller/cron.js
+++ src/controller/cron.js
@@ -10,7 +10,7 @@ module.exports = class extends think.Controller {
       .map(name => name.split('.js')[0]);
 
     const twoMonthAgo = moment().subtract(2, 'months').format('YYYY-MM-DD 00:00:00');
-    await Promise.all(metrics.map(async metric => this.model(metric).where({
+    await Promise.all(metrics.map(async metric => this.model('pref_' + metric).where({
       create_time: ['<', twoMonthAgo]
     }).delete()));
     think.logger.info(`清除 ${twoMonthAgo} 之前的老数据成功`);
