3d980ec7f528ffb5209cb65a0d499c20c20c2c66
lizheming <secretlzm007@gmail.com>
2018-07-01 16:13:38 +0800

    fix clean crontab delete much data

--- src/controller/cron.js
+++ src/controller/cron.js
@@ -10,10 +10,35 @@ module.exports = class extends think.Controller {
       .map(name => name.split('.js')[0]);
 
     const twoMonthAgo = moment().subtract(2, 'months').format('YYYY-MM-DD 00:00:00');
-    await Promise.all(metrics.map(async metric => this.model('pref_' + metric).where({
-      create_time: ['<', twoMonthAgo]
-    }).delete()));
-    think.logger.info(`清除 ${twoMonthAgo} 之前的老数据成功`);
+    const pageSize = 2500;
+
+    for (const metric of metrics) {
+      const tableName = 'perf_' + metric;
+      const model = this.model(tableName);
+      let page = 1;
+      while (true) {
+        // 分片删除避免一次性删除过多数据
+        const {totalPages, data} = await model.where({
+          create_time: ['<', twoMonthAgo]
+        }).page(page, pageSize)
+          .field('id')
+          .countSelect();
+
+        if (think.isEmpty(data)) {
+          break;
+        }
+
+        await model.where({id: ['IN', data.map(({id}) => id)]}).delete();
+
+        if (totalPages === page) {
+          break;
+        } else {
+          page += 1;
+        }
+      }
+      think.logger.info(`清除 ${twoMonthAgo} 之前的 ${tableName} 老数据成功`);
+    }
+
     return this.success();
   }
 };
