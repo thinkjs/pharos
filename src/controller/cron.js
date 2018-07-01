03aa84d9f7a36b483fda605ad786c47d10112d79
lizheming <secretlzm007@gmail.com>
2018-07-01 14:20:13 +0800

    fix clean not excute bug

--- src/controller/cron.js
+++ src/controller/cron.js
@@ -10,9 +10,9 @@ module.exports = class extends think.Controller {
       .map(name => name.split('.js')[0]);
 
     const twoMonthAgo = moment().subtract(2, 'months').format('YYYY-MM-DD 00:00:00');
-    await metrics.map(async metric => this.model(metric).where({
+    await Promise.all(metrics.map(async metric => this.model(metric).where({
       create_time: ['<', twoMonthAgo]
-    }).delete());
+    }).delete()));
     think.logger.info(`清除 ${twoMonthAgo} 之前的老数据成功`);
     return this.success();
   }
