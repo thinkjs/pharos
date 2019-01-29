aa4e9b4745facbc6a06ae78b6ef26e78c253892d
lizheming <i@imnerd.org>
2019-01-29 20:05:01 +0800

    rm backend compile

--- development.js
+++ development.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const Application = require('thinkjs');
 const watcher = require('think-watcher');
 
 const instance = new Application({
+  APP_PATH: path.join(__dirname, 'src'),
   ROOT_PATH: __dirname,
   watcher: watcher,
   env: 'development'
--- production.js
+++ production.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const Application = require('thinkjs');
 
 const instance = new Application({
+  APP_PATH: path.join(__dirname, 'src'),
   ROOT_PATH: __dirname,
   proxy: true, // use proxy
   env: 'production'
