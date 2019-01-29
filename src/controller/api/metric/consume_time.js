f083ffe4394adcc9b3fa056e775afa526f7eb870
lizheming <i@imnerd.org>
2019-01-29 20:05:57 +0800

    add api/metric/js_error

--- src/controller/api/metric/consume_time.js
+++ src/controller/api/metric/consume_time.js
@@ -1,26 +1,5 @@
 const Base = require('./base');
 
-const FIVE_MINS = 5 * 60 * 1000;
-const ONE_HOUR = FIVE_MINS * 12;
-const ONE_DAY = ONE_HOUR * 24;
-const BETWEEN = {
-  day: {
-    delta: ONE_DAY,
-    format: 'YYYY-MM-DD',
-    transform: 'YYYY-MM-DD 00:00:00'
-  },
-  hour: {
-    delta: ONE_HOUR,
-    format: 'MM-DD HH:00',
-    transform: 'YYYY-MM-DD HH:00:00'
-  },
-  mins: {
-    delta: FIVE_MINS,
-    format: 'MM-DD HH:mm',
-    transform: 'YYYY-MM-DD HH:mm:00'
-  }
-};
-
 module.exports = class extends Base {
   constructor(...args) {
     super(...args);
@@ -171,17 +150,5 @@ module.exports = class extends Base {
 
   postAction() {
     return this.dataCollection('consume_time', ['perf', 'section']);
-  }
-
-  generateCates(start_time, end_time, type = 'day') {
-    const { delta, format, transform } = BETWEEN[type] || BETWEEN['day'];
-    const startTime = new Date(think.datetime(new Date(start_time), transform)).getTime();
-    const endTime = new Date(think.datetime(new Date(end_time), transform)).getTime();
-
-    const times = [];
-    for (let time = startTime; time < endTime; time += delta) {
-      times.push(think.datetime(new Date(time), format));
-    }
-    return times;
   }
 };
--- src/controller/api/metric/js_error.js
+++ src/controller/api/metric/js_error.js
@@ -6,6 +6,50 @@ module.exports = class extends Base {
   }
 
   async getAction() {
+    const {
+      site_id,
+      site_page_id,
+      start_time,
+      end_time
+    } = this.get();
+
+    const where = { site_id, create_time: { '>=': start_time, '<': end_time } };
+    if (site_page_id) { where.site_page_id = site_page_id }
+
+    const data = await this.modelInstance
+      .where(where)
+      .order('create_time DESC')
+      .select();
+
+    const categories = this.generateCates(start_time, end_time, 'mins');
+    const series = {};
+    const total = {};
+
+    for (let i = 0; i < data.length; i++) {
+      const { error, count, create_time } = data[i];
+      const time = think.datetime(create_time, 'MM-DD HH:mm');
+      if (!series[error]) {
+        series[error] = {};
+      }
+      if (!series[error][time]) {
+        series[error][time] = 0;
+      }
+      if (!total[time]) {
+        total[time] = 0;
+      }
+
+      series[error][time] += count;
+      total[time] = 0;
+    }
+    series.Total = total;
+    const result = [];
+    for (const k in series) {
+      result.push({
+        name: k,
+        data: categories.map(cate => series[k][cate])
+      });
+    }
+    return this.success({ categories, series: result });
   }
 
   postAction() {
