1df7a5c73c3104fea502eb2b8a2fdf052514babe
lizheming <i@imnerd.org>
2019-01-29 20:06:09 +0800

    init page

--- www/static/src/utils/menus.js
+++ www/static/src/utils/menus.js
@@ -1,95 +1,105 @@
 const menus = [
   {
-    name:'项目列表',
-    key:'site-list',
-    url:'/site/list',
-    hideSlider:true,
-    hideProject:true,
-    children:[
+    name: '项目列表',
+    key: 'site-list',
+    url: '/site/list',
+    hideSlider: true,
+    hideProject: true,
+    children: [
       {
-        name:'项目列表',
-        icon:'database',
-        url:'/site/list'
+        name: '项目列表',
+        icon: 'database',
+        url: '/site/list'
       }
     ]
   },
   {
-    name:'性能分析',
+    name: '性能分析',
     key: 'perf',
-    url:'/perf/overview',
-    children:[
+    url: '/perf/overview',
+    children: [
       {
-        name:'概览',
-        icon:'desktop',
-        url:'/perf/overview',
+        name: '概览',
+        icon: 'desktop',
+        url: '/perf/overview'
       },
       {
-        name:'数据分析',
-        icon:'line-chart',        
-        children:[
+        name: '数据分析',
+        icon: 'line-chart',
+        children: [
           {
-            name:'按时段',
-            url:'/perf/specific/hour',
+            name: '按时段',
+            url: '/perf/specific/hour'
           },
           {
-            name:'按日期',
-            url:'/perf/specific/day'
+            name: '按日期',
+            url: '/perf/specific/day'
           },
           {
-            name:'按耗时区间',
-            url:'/perf/specific/interval'
+            name: '按耗时区间',
+            url: '/perf/specific/interval'
           },
           {
-            name:'按操作系统',
-            url:'/perf/specific/os'
+            name: '按操作系统',
+            url: '/perf/specific/os'
           },
           {
-            name:'按浏览器',
-            url:'/perf/specific/browser'
-          },
+            name: '按浏览器',
+            url: '/perf/specific/browser'
+          }
           // {
           //   name:'按地域',
           //   url:'/perf/specific/region'
           // },
         ]
+      },
+      {
+        name: '报错监控',
+        icon: 'bell',
+        children: [
+          {
+            name: '实时数据',
+            url: '/perf/js-error'
+          }
+        ]
       }
     ]
   },
   {
-    name:'项目管理',
-    key:'site',
-    url:'/site/field',
-    children:[
+    name: '项目管理',
+    key: 'site',
+    url: '/site/field',
+    children: [
       {
-        name:'性能指标',
-        icon:'bars',
-        url:'/site/field'
+        name: '性能指标',
+        icon: 'bars',
+        url: '/site/field'
       },
       {
-        name:'用户管理',
-        icon:'user',
-        url:'/site/user'
+        name: '用户管理',
+        icon: 'user',
+        url: '/site/user'
       },
       {
-        name:'项目配置',
-        icon:'calculator',
-        url:'/site/setting'
+        name: '项目配置',
+        icon: 'calculator',
+        url: '/site/setting'
       }
     ]
   },
   {
-    name:'基础设置',
+    name: '基础设置',
     hidden: window.USER.status !== 1,
-    key:'setting',
-    url:'/setting/user',    
-    children:[
+    key: 'setting',
+    url: '/setting/user',
+    children: [
       {
-        name:'用户管理',
-        icon:'user',
-        url:'/setting/user'
+        name: '用户管理',
+        icon: 'user',
+        url: '/setting/user'
       }
     ]
   }
-]
+];
 
 export default menus;
