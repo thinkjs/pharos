9d8cc847ceeae8be3f1c7037aaae2fbe5674d024
lizheming <i@imnerd.org>
2019-05-05 15:23:40 +0800

    clean workspace

--- www/static/src/utils/helper.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getDataFromLs(key){
-    let data = localStorage.getItem(key);
-    if(data && data !== 'undefined'){
-      data = JSON.parse(data);
-    }
-    return data;
-  }
-}
