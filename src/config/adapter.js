bfbb7273f2da9765f59a47f8970893a850c552b2
Hu Jian <color.dove@gmail.com>
2019-05-06 16:45:23 +0800

    feat(init): init app

--- src/config/adapter.js
+++ src/config/adapter.js
@@ -36,13 +36,13 @@ exports.model = {
   },
   mysql: {
     handle: mysql,
-    database: '',
-    prefix: 'think_',
+    database: 'monitor',
+    prefix: 'ph_',
     encoding: 'utf8',
     host: '127.0.0.1',
     port: '',
     user: 'root',
-    password: 'root',
+    password: '123456',
     dateStrings: true
   }
 };
