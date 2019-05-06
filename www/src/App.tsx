3367705a077c4541bc513acd337a531984929b22
linxd <linxd@touzhijia.com>
2019-05-06 16:02:49 +0800

    优化初始项目，添加必要插件

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,24 +1,22 @@
 import * as React from "react";
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import { PageA } from './pages/PageA';
+import Login from './pages/login';
 import { PageB } from './pages/PageB';
 
 
-export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
-}
+// export interface Props {
+//   name: string;
+//   enthusiasmLevel?: number;
+// }
 
-function App({ name, enthusiasmLevel = 1 }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
-  }
+// interface AppProps { }
 
+function App() {
   return (
     <>
       <HashRouter>
         <Switch>
-          <Route exact={true} path="/" component={PageA} />
+          <Route exact={true} path="/" component={Login} />
           <Route path="/pageB" component={PageB} />
         </Switch>
       </HashRouter>
--- www/src/index.tsx
+++ www/src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App name="app" />,
+  <App />,
   document.getElementById('root')
 );
