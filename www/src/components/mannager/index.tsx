a316dd45e2216ee80c3a3e4d81d62045c4d6fe93
刘宏杰 <1837337881@qq.com>
2019-05-08 19:09:48 +0800

    clear test component

--- www/src/components/mannager/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-
-export default class Mananger extends React.Component {
-  render() {
-    return <div>maanger页面</div>;
-  }
-}
