cd3a0aa9952b1af6d680893d718d038392e0d697
刘宏杰 <1837337881@qq.com>
2019-05-08 18:53:50 +0800

    add less & router

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,62 +1,76 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var webpack = require("webpack");
-var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+var path = require('path');
 
 var basePath = __dirname;
 
 module.exports = {
-  context: path.join(basePath, "src"),
+  context: path.join(basePath, 'src'),
   resolve: {
-    extensions: [".js", ".ts", ".tsx"]
+    extensions: ['.js', '.ts', '.tsx']
   },
-  entry: ["@babel/polyfill", "./index.tsx"],
+  entry: ['@babel/polyfill', './index.tsx'],
   output: {
-    path: path.join(basePath, "dist"),
-    filename: "bundle.js"
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
-    contentBase: "./dist", // Content base
+    contentBase: './dist', // Content base
     inline: true, // Enable watch and live reload
-    host: "localhost",
+    host: 'localhost',
     port: 8080,
-    stats: "errors-only"
+    stats: 'errors-only'
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
+        loader: 'awesome-typescript-loader',
         options: {
           useBabel: true,
-          babelCore: "@babel/core" // needed for Babel v7
+          babelCore: '@babel/core' // needed for Babel v7
         }
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "assets/img/[name].[ext]?[hash]"
+          name: 'assets/img/[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader' // compiles Less to CSS
+          }
+        ]
       }
     ]
   },
   plugins: [
-    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    // Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: "index.html", //Name of file in ./dist/
-      template: "index.html", //Name of template in ./src
+      filename: 'index.html', // Name of file in ./dist/
+      template: 'index.html', // Name of template in ./src
       hash: true
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css'
     })
   ]
 };
