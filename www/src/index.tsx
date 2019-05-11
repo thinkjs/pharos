9b9aafec1dc3d06c7ed9d62255f9bc372e3e0524
linxd <linxd@touzhijia.com>
2019-05-11 09:07:31 +0800

    fulfill login

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,7 +1,6 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
 import App from './App'
 import { Router } from 'react-router-dom'
 import history from './components/history'
