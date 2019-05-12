73bda66c3690bce4483215039bd663f19d0dc510
lizheming <i@imnerd.org>
2019-05-12 13:52:58 +0800

    remove unused install check

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,10 +1,5 @@
 module.exports = class extends think.Controller {
   async __before() {
-    if (this.ctx.action !== 'install') {
-      if (!global.isInstalled) {
-        return this.redirect('/index/install');
-      }
-    }
     // session
     const userInfo = await this.session('userInfo') || {};
 
