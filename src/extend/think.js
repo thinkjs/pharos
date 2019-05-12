206969fba79a17f6ae2d82cb20fc68d4f36f039e
lizheming <i@imnerd.org>
2019-05-12 18:03:09 +0800

    add data from log file

--- src/extend/think.js
+++ src/extend/think.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const preventMessage = 'PREVENT_NEXT_PROCESS';
+
 module.exports = {
   prevent() {
     throw new Error(preventMessage);
   },
   isPrevent(err) {
     return think.isError(err) && err.message === preventMessage;
-  }
+  },
+  RUNTIME_PATH: path.join(think.ROOT_PATH, 'runtime'),
+  RESOURCE_PATH: path.join(think.ROOT_PATH, 'www')
 };
