a0b72c75a28c1b6d1387c3c5a0b4970abda17689
Hu Jian <color.dove@gmail.com>
2019-05-15 14:14:46 +0800

    feat(adapter): add adapter file

---
+++ src/config/adapter.js
@@ -0,0 +1,98 @@
+const fileCache = require('think-cache-file');
+const nunjucks = require('think-view-nunjucks');
+const fileSession = require('think-session-file');
+const mysql = require('think-model-mysql');
+const path = require('path');
+const { DateFile } = require('think-logger3');
+const isDev = think.env === 'development';
+
+/**
+ * cache adapter config
+ * @type {Object}
+ */
+exports.cache = {
+  type: 'file',
+  common: {
+    timeout: 24 * 60 * 60 * 1000 // millisecond
+  },
+  file: {
+    handle: fileCache,
+    cachePath: path.join(think.ROOT_PATH, 'runtime/cache'), // absolute path is necessarily required
+    pathDepth: 1,
+    gcInterval: 24 * 60 * 60 * 1000 // gc interval
+  }
+};
+
+/**
+ * model adapter config
+ * @type {Object}
+ */
+exports.model = {
+  type: 'mysql',
+  common: {
+    logConnect: isDev,
+    logSql: isDev,
+    logger: msg => think.logger.info(msg)
+  },
+  mysql: {
+    handle: mysql,
+    database: 'monitor',
+    prefix: 'ph_',
+    encoding: 'utf8',
+    host: '127.0.0.1',
+    port: '',
+    user: 'root',
+    password: '123456',
+    dateStrings: true
+  }
+};
+
+/**
+ * session adapter config
+ * @type {Object}
+ */
+exports.session = {
+  type: 'file',
+  common: {
+    cookie: {
+      name: 'thinkjs'
+      // keys: ['werwer', 'werwer'],
+      // signed: true
+    }
+  },
+  file: {
+    handle: fileSession,
+    sessionPath: path.join(think.ROOT_PATH, 'runtime/session')
+  }
+};
+
+/**
+ * view adapter config
+ * @type {Object}
+ */
+exports.view = {
+  type: 'nunjucks',
+  common: {
+    viewPath: path.join(think.ROOT_PATH, 'view'),
+    sep: '_',
+    extname: '.html'
+  },
+  nunjucks: {
+    handle: nunjucks
+  }
+};
+
+exports.logger = {
+  type: isDev ? 'console' : 'dateFile',
+  dateFile: {
+    handle: DateFile,
+    absolute: true,
+    pattern: '-yyyy-MM-dd',
+    alwaysIncludePattern: true,
+    filename: path.join(think.ROOT_PATH, 'logs/xx.log'),
+    layout: {
+      type: 'pattern',
+      pattern: '%[[%d] [%z] [%p]%] - %m'
+    }
+  }
+};
