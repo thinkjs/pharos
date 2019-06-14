34a7120f8c87092ad26d00f25b30d80ebc7465c0
linxd <linxd@touzhijia.com>
2019-06-14 17:53:42 +0800

    换回ts-loader

--- www/webpack/config.common.js
+++ www/webpack/config.common.js
@@ -81,7 +81,7 @@ module.exports = {
     rules: [{
       test: /\.(ts|tsx)$/,
       exclude: /node_modules/,
-      loader: ['awesome-typescript-loader']
+      loader: ['babel-loader', 'ts-loader']
     }, {
       test: /\.css$/,
       use: cssLoaders(),
@@ -104,16 +104,7 @@ module.exports = {
         name: '[path][name].[ext]&limit=200000'
       }
     },
-    { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
-  },
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
   },
   plugins: [
     ...htmlEntries,
