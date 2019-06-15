47898f31da49add374b3d9f01d2aab6fa3a43ed1
lizheming <i@imnerd.org>
2019-06-15 12:56:37 +0800

    增加 alarm 的假接口

---
+++ src/controller/api/site/alarm.js
@@ -0,0 +1,31 @@
+const Base = require('../base.js');
+
+module.exports = class extends Base {
+  constructor(...args) {
+    super(...args);
+    this.modelInstance = this.model('site_alarm');
+  }
+
+  getAction() {
+    return this.success({
+      page: 1,
+      totalPages: 1,
+      pageSize: 10,
+      data: [
+        {
+          id: 1,
+          name: '报警测试数据',
+          metric: {
+            id: 3,
+            name: '报错监控'
+          },
+          condition: {
+            count: 10,
+            expression: '>',
+            limit: 0.3
+          }
+        }
+      ]
+    })
+  }
+};
