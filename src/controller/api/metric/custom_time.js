f7ca56b09de3ed1d5758b928944c572de767f17c
lizheming <i@imnerd.org>
2019-06-16 12:11:35 +0800

    重构数据归并部分

--- src/controller/api/metric/custom_time.js
+++ src/controller/api/metric/custom_time.js
@@ -27,50 +27,65 @@ module.exports = class extends Base {
     let categories;
     switch (type) {
       case 'day':
-        categories = this.generateCates(start_time, end_time, type);
-        series = await this.groupWithCustom(site_id, metric_id, metric, data, customData => {
-          const map = new Map();
-          for (let i = 0; i < customData.data.length; i++) {
-            const create_time = customData.data[i].create_time.split(' ')[0];
-            if (map.has(create_time)) {
-              map.set(create_time, (+map.get(create_time)) + (+customData.data[i][metric]));
-            } else {
-              map.set(create_time, (+customData.data[i][metric]));
-            }
-          }
-          const addedData = map;
-          const result = {
-            data: []
-          };
-          result.name = customData.name;
-          result.data = new Array(categories.length).fill(null);
-          categories.map((cat, index) => {
-            for (let item of addedData.entries()) {
-              if (item[0].includes(cat)) {
-                result.data[index] = item[1];
-              }
-            }
-          })
-          return [result];
-        });
-        break;
+      // categories = this.generateCates(start_time, end_time, type);
+      // series = await this.groupWithCustom(site_id, metric_id, metric, data, customData => {
+      //   const map = new Map();
+      //   for (let i = 0; i < customData.data.length; i++) {
+      //     const create_time = customData.data[i].create_time.split(' ')[0];
+      //     if (map.has(create_time)) {
+      //       map.set(create_time, (+map.get(create_time)) + (+customData.data[i][metric]));
+      //     } else {
+      //       map.set(create_time, (+customData.data[i][metric]));
+      //     }
+      //   }
+      //   const addedData = map;
+      //   const result = {
+      //     data: []
+      //   };
+      //   result.name = customData.name;
+      //   result.data = new Array(categories.length).fill(null);
+      //   categories.map((cat, index) => {
+      //     for (let item of addedData.entries()) {
+      //       if (item[0].includes(cat)) {
+      //         result.data[index] = item[1];
+      //       }
+      //     }
+      //   })
+      //   return [result];
+      // });
+      // break;
       // case 'hour':
       case 'mins':
         categories = this.generateCates(start_time, end_time, type);
-        series = await this.groupWithCustom(site_id, metric_id, metric, data, customData => {
-          const result = {
-            data: []
+        series = await this.groupWithCustom(site_id, metric_id, metric, data, ({ data, name }) => {
+          const result = {};
+          for (let i = 0; i < data.length; i++) {
+            const date = think.datetime(
+              new Date(data[i].create_time),
+              Base.BETWEEN[type].format
+            );
+            if (!result[date]) {
+              result[date] = { time: 0, count: 0 };
+            }
+            result[date].time += data[i].time;
+            result[date].count += data[i].count;
+          }
+
+          const output = [];
+          for (let i = 0; i < categories.length; i++) {
+            const cat = categories[i];
+            if (think.isEmpty(result[cat])) {
+              output.push(null);
+              continue;
+            }
+
+            output.push(this.avg(result[cat], 0));
+          }
+
+          return {
+            name,
+            data: output
           };
-          result.name = customData.name;
-          result.data = new Array(categories.length).fill(null);
-          categories.map((cat, index) => {
-            customData.data.map(item => {
-              if (item.create_time.includes(cat)) {
-                result.data[index] = +item[metric];
-              }
-            })
-          })
-          return [result];
         });
         break;
       default:
