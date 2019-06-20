1978b5b1ee7fe2d98e54e737e3430ad061cbf7b0
Hu Jian <color.dove@gmail.com>
2019-06-20 18:41:01 +0800

    feat(metric): 增加同比/环比数据

---
+++ src/logic/api/metric/ratio.js
@@ -0,0 +1,73 @@
+const Base = require('./base');
+module.exports = class extends Base {
+  /**
+   * @api {GET} /metric/custom_time 获取某段时间自定义监控的数据
+   * @apiGroup Metric
+   * @apiVersion  0.0.1
+   *
+   * @apiParam  {Int}     site_id 网站ID
+   * @apiParam  {Int}  metric_id 监控项ID
+   * 
+   * @apiSuccessExample {json} 成功返回
+   * {
+   *   errno: 0,
+   *   data: {
+   *       today: {
+   *         category: ["05-24 00:00", "05-24 00:05",],
+   *         series: [
+   *           {
+   *             name: 'Chrome',
+   *             data: [50, 20]
+   *           }
+   *         ]
+   *       },
+   *       yesterday: {
+   *         category: ["05-24 00:00", "05-24 00:05",],
+   *         series: [
+   *           {
+   *             name: 'Chrome',
+   *             data: [50, 20]
+   *           }
+   *         ]
+   *       },
+   *       this_week: {
+   *         category: ["05-24 00:00", "05-24 00:05",],
+   *         series: [
+   *           {
+   *             name: 'Chrome',
+   *             data: [50, 20]
+   *           }
+   *         ]
+   *       },
+   *       last_week: {
+   *         category: ["05-24 00:00", "05-24 00:05",],
+   *         series: [
+   *           {
+   *             name: 'Chrome',
+   *             data: [50, 20]
+   *           }
+   *         ]
+   *       }
+   *   },
+   *   errmsg: ''
+   * }
+   */
+
+
+  getAction() {
+    const ONE_DAY = 24 * 3600000;
+    const ONE_MONTH = ONE_DAY * 30;
+    const INTERVAL = this.get('type') === 'day' ? ONE_MONTH : ONE_DAY;
+
+    this.rules = {
+      site_id: {
+        required: true,
+        int: true
+      },
+      metric_id: {
+        required: true,
+        int: true
+      },
+    };
+  }
+};
