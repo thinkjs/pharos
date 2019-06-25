3d7834e56a20050b1a3e406f1c530f9e7256fd25
刘宏杰 <1837337881@qq.com>
2019-06-25 23:35:48 +0800

    change sid

--- www/src/pages/Project/Embed.tsx
+++ www/src/pages/Project/Embed.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { observer, inject } from 'mobx-react';
 import { Button } from 'antd'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { message } from 'antd'
@@ -7,38 +8,35 @@ import history from '@utils/history'
 import './embed.less'
 
 
+@inject('projectStore') @observer
+class Embed extends React.Component<any, any> {
 
-const Embed = () => {
-  if (!localStorage.getItem('projectId')) {
-    history.push('/project/create')
-    return null
+  componentDidMount() {
+    const { projectStore } = this.props
+    projectStore.getList()
   }
 
-  const text = `<script
-  src="//lib.baomitu.com/lightkeeper/latest/pharos.min.js"
-  data-siteid=${localStorage.getItem('projectId')}
-  data-host="//pharos.baomitu.com"  
-  >
-</script>
-<script type="text/javascript">
-  window.addEventListener('load', function() {
-    pharos.monitor();
-  });
-</script>`
-
-  return (
-    <div className="embed-wrap">
-      <div className="embed-wrap-content">
-        <div className="copy-btn" onClick={() => message.info('已复制到剪贴板')}>
-          <CopyToClipboard text={text}>
-            <span className="btn-flat">复制</span>
-          </CopyToClipboard>
+  render() {
+    if (!localStorage.getItem('projectId')) {
+      history.push('/project/create')
+      return null
+    }
+    const { projectStore } = this.props
+    const { jsString } = projectStore
+    return (
+      <div className="embed-wrap">
+        <div className="embed-wrap-content">
+          <div className="copy-btn" onClick={() => message.info('已复制到剪贴板')}>
+            <CopyToClipboard text={jsString}>
+              <span className="btn-flat">复制</span>
+            </CopyToClipboard>
+          </div>
+          <pre>{jsString}</pre>
         </div>
-        <pre>{text}</pre>
+        <Button type="primary"><Link to="/alarm">进入项目</Link></Button>
       </div>
-      <Button type="primary"><Link to="/alarm">进入项目</Link></Button>
-    </div>
-  )
+    )
+  }
 }
 
 export default Embed
--- www/src/pages/Site/Config/index.tsx
+++ www/src/pages/Site/Config/index.tsx
@@ -5,33 +5,22 @@ import { observer, inject } from 'mobx-react';
 
 import './style.less'
 
-const text = `<script
-  src="//lib.baomitu.com/lightkeeper/latest/pharos.min.js"
-  data-siteid=${localStorage.getItem('projectId')}
-  data-host="//pharos.baomitu.com"  
-  >
-</script>
-<script type="text/javascript">
-  window.addEventListener('load', function() {
-    pharos.monitor();
-  });
-</script>`
 
 @inject('projectStore') @observer
 class Config extends React.Component<any, any> {
 
   render() {
     const { projectStore } = this.props
-    const { showDeleteModal, setShowDeleteModal } = projectStore
+    const { showDeleteModal, setShowDeleteModal, jsString } = projectStore
     return (
       <div className="config-wrap">
         <div className="config-wrap-content">
           <div className="copy-btn" onClick={() => message.info('已复制到剪贴板')}>
-            <CopyToClipboard text={text}>
+            <CopyToClipboard text={jsString}>
               <span className="btn-flat">复制</span>
             </CopyToClipboard>
           </div>
-          <pre>{text}</pre>
+          <pre>{jsString}</pre>
         </div>
         {/* <div className="group-item">
           <div className="group-item-label">删除项目：
