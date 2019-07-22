6e4b1c6c9a3b3e2ae4b2477525f794d068f8c450
linxd <linxd@touzhijia.com>
2019-07-22 18:29:21 +0800

    change siteId to projectId

--- www/src/pages/Site/Strategy/index.tsx
+++ www/src/pages/Site/Strategy/index.tsx
@@ -1,43 +1,42 @@
-import * as React from 'react';
-import { observer, inject } from 'mobx-react';
-import { Table, Divider, Popconfirm, Button } from 'antd';
-import StrateFormModal from './modal'
+import * as React from "react";
+import { observer, inject } from "mobx-react";
+import { Table, Divider, Popconfirm, Button } from "antd";
+import StrateFormModal from "./modal";
 
-
-@inject('strategyStore') @observer
+@inject("strategyStore")
+@observer
 class Strategy extends React.Component<any, any> {
-
   state = {
     columns: [
       {
-        title: '策略名称',
-        dataIndex: 'name',
-        key: 'name',
+        title: "策略名称",
+        dataIndex: "name",
+        key: "name"
       },
       {
-        title: '监控次数',
-        dataIndex: 'count',
-        key: 'count',
+        title: "监控次数",
+        dataIndex: "count",
+        key: "count"
       },
       {
-        title: '关系',
-        dataIndex: 'expression',
-        key: 'expression',
+        title: "关系",
+        dataIndex: "expression",
+        key: "expression"
       },
       {
-        title: '阈值',
-        dataIndex: 'limit',
-        key: 'limit',
+        title: "阈值",
+        dataIndex: "limit",
+        key: "limit"
       },
       {
-        title: '创建时间',
-        dataIndex: 'create_time',
-        key: 'create_time',
+        title: "创建时间",
+        dataIndex: "create_time",
+        key: "create_time"
       },
       {
-        title: '操作',
-        key: 'action',
-        render: (text) => {
+        title: "操作",
+        key: "action",
+        render: text => {
           return (
             <span>
               <span onClick={() => this.handleEdit(text)}>编辑</span>
@@ -51,59 +50,65 @@ class Strategy extends React.Component<any, any> {
                 删除
               </Popconfirm>
             </span>
-          )
-        },
+          );
+        }
       }
     ]
-  }
-  handleEdit = (text) => {
-    this.handleBtnClick()
-    const { strategyStore } = this.props
-    const { setDefaultVal } = strategyStore
-    setDefaultVal(text)
-  }
-  handleDelete = (text) => {
-    const { strategyStore } = this.props
-    const { deleteItem } = strategyStore
+  };
+  handleEdit = text => {
+    this.handleBtnClick();
+    const { strategyStore } = this.props;
+    const { setDefaultVal } = strategyStore;
+    setDefaultVal(text);
+  };
+  handleDelete = text => {
+    const { strategyStore } = this.props;
+    const { deleteItem } = strategyStore;
 
-    deleteItem(text.id)
-  }
+    deleteItem(text.id);
+  };
   componentDidMount() {
-    this.props.strategyStore.getList()
+    this.props.strategyStore.getList();
   }
 
   handleBtnClick = () => {
-    const { strategyStore } = this.props
-    const { showModal } = strategyStore
-    showModal()
-  }
-  createList = (arr) => arr.map(item => {
-    let conditions = JSON.parse(item.conditions)
-    return {
-      ...item,
-      ...conditions
-    }
-  })
+    const { strategyStore } = this.props;
+    const { showModal } = strategyStore;
+    showModal();
+  };
+  createList = arr =>
+    arr.map(item => {
+      let conditions = JSON.parse(item.conditions);
+      return {
+        ...item,
+        ...conditions
+      };
+    });
   render() {
-    const { columns } = this.state
-    const { strategyStore } = this.props
-    let list: any = this.createList(strategyStore.list)
+    const { columns } = this.state;
+    const { strategyStore } = this.props;
+    let list: any = this.createList(strategyStore.list);
     return (
       <div>
-        <div style={{ height: '40px', marginBottom: '10px' }}>
-          <Button type="primary" style={{ float: 'right' }} onClick={this.handleBtnClick}>添加监控策略</Button>
+        <div style={{ height: "40px", marginBottom: "10px" }}>
+          <Button
+            type="primary"
+            style={{ float: "right" }}
+            onClick={this.handleBtnClick}
+          >
+            添加监控策略
+          </Button>
         </div>
         <Table
           bordered={true}
           dataSource={list}
           columns={columns}
-          rowKey='id'
+          rowKey="id"
         />
         <StrateFormModal />
       </div>
-    )
+    );
   }
 }
 
-
-export default Strategy
+export default Strategy;
--- www/src/pages/Site/store/strategy.ts
+++ www/src/pages/Site/store/strategy.ts
@@ -1,46 +1,45 @@
-import { message } from 'antd';
-import { observable, action, computed } from 'mobx';
-import axios from '@utils/axios';
+import { message } from "antd";
+import { observable, action, computed } from "mobx";
+import axios from "@utils/axios";
 
 class Strategy {
   rootStore;
   constructor(rootStore) {
-    this.rootStore = rootStore
+    this.rootStore = rootStore;
   }
   @computed get siteId() {
-    return localStorage.getItem('siteId')
+    return localStorage.getItem("projectId");
   }
-  @observable list: any = []
-  @observable modalStatus = false
+  @observable list: any = [];
+  @observable modalStatus = false;
   @observable defaultVal: any = {
-    name: '',
-    display_name: '',
+    name: "",
+    display_name: "",
     count: 1,
-    expression: '=',
-    limit: ''
-  }
+    expression: "=",
+    limit: ""
+  };
 
   @action hideModal = () => {
-    this.modalStatus = false
-    this.setDefaultVal({})
-  }
-  @action showModal = () => this.modalStatus = true
-  @action setDefaultVal = (obj) => this.defaultVal = obj
-  @action setList = (list: any) => this.list = list
+    this.modalStatus = false;
+    this.setDefaultVal({});
+  };
+  @action showModal = () => (this.modalStatus = true);
+  @action setDefaultVal = obj => (this.defaultVal = obj);
+  @action setList = (list: any) => (this.list = list);
   @action getList = async (criteria?: any) => {
     let params: any;
-    params = criteria ? criteria : { page: '1', pagesize: 50 }
-    try {
-      const { data } = await axios.get(`/api/site/${this.siteId}/alarm`, { params })
-      if (data.errno === 0) {
-        this.setList(data.data.data)
-      } else {
-        message.error('后台出错请稍后再试')
-      }
-    } catch (e) {
-      message.error('获取列表失败');
+    params = criteria ? criteria : { page: "1", pagesize: 50 };
+    const { data } = await axios.get(`/api/site/${this.siteId}/alarm`, {
+      params
+    });
+    console.log(15, data.data.data);
+    if (data.errno === 0) {
+      this.setList(data.data.data);
+    } else {
+      message.error("后台出错请稍后再试");
     }
-  }
+  };
 
   @action addItem = async (params?: any) => {
     let data = {
@@ -51,16 +50,18 @@ class Strategy {
         expression: params.expression,
         limit: params.limit
       })
-    }
-    let res: any = await axios.post(`/api/site/${this.siteId}/alarm`, { ...data })
+    };
+    let res: any = await axios.post(`/api/site/${this.siteId}/alarm`, {
+      ...data
+    });
     if (res.data.errno === 0) {
-      this.getList()
-      this.hideModal()
+      this.getList();
+      this.hideModal();
     } else {
-      message.error('后台出错请稍后再试')
-      this.hideModal()
+      message.error("后台出错请稍后再试");
+      this.hideModal();
     }
-  }
+  };
   @action changeItem = async (params?: any) => {
     let data = {
       name: params.name,
@@ -70,25 +71,27 @@ class Strategy {
         expression: params.expression,
         limit: params.limit
       })
-    }
-    let res: any = await axios.put(`/api/site/${this.siteId}/alarm/${this.defaultVal.id}`, { ...data })
+    };
+    let res: any = await axios.put(
+      `/api/site/${this.siteId}/alarm/${this.defaultVal.id}`,
+      { ...data }
+    );
     if (res.data.errno === 0) {
-      this.getList()
-      this.hideModal()
+      this.getList();
+      this.hideModal();
     } else {
-      message.error('后台出错请稍后再试')
-      this.hideModal()
+      message.error("后台出错请稍后再试");
+      this.hideModal();
     }
-  }
+  };
   @action deleteItem = async (id?: any) => {
-    let res: any = await axios.delete(`/api/site/${this.siteId}/alarm/${id}`)
+    let res: any = await axios.delete(`/api/site/${this.siteId}/alarm/${id}`);
     if (res.data.errno === 0) {
-      this.getList()
+      this.getList();
     } else {
-      message.error('后台出错请稍后再试')
+      message.error("后台出错请稍后再试");
     }
-  }
+  };
 }
 
-
-export default Strategy
+export default Strategy;
