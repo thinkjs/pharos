facf808c6b666fe8bf9e19c58a67b2c12556e912
linxd <linxd@touzhijia.com>
2019-08-05 16:22:32 +0800

    add type and modify time format

--- www/src/config/domain.ts
+++ www/src/config/domain.ts
@@ -1,9 +1,9 @@
-import env from './env'
+import env from "./env";
 const apiUrlMap = {
-  'development': 'http://api.pharos.net/',
-  'test': 'http://api.pharos.net/',
-  'production': '/',
-}
+  development: "http://pharos2.baomitu.com/",
+  test: "http://pharos2.baomitu.com/",
+  production: "/"
+};
 
-export const baseURL = apiUrlMap[env]
+export const baseURL = apiUrlMap[env];
 // export const rootDomain = _rootDomainMap[env] || _rootDomainMap.test
--- www/src/pages/Monitor/Error/Picture/index.tsx
+++ www/src/pages/Monitor/Error/Picture/index.tsx
@@ -1,72 +1,70 @@
-import * as React from 'react';
-import { Layout, Cascader } from 'antd'
-import { observer, inject } from 'mobx-react';
+import * as React from "react";
+import { Layout, Cascader } from "antd";
+import { observer, inject } from "mobx-react";
 import * as Highcharts from "highcharts";
-import HighchartsReact from 'highcharts-react-official'
-import axios from '@utils/axios'
-import Fieldset from '@components/Fieldset'
-import moment from '@utils/moment'
-import { DatePicker } from 'antd';
-import './style.less'
+import HighchartsReact from "highcharts-react-official";
+import axios from "@utils/axios";
+import Fieldset from "@components/Fieldset";
+import moment from "@utils/moment";
+import { DatePicker, Radio } from "antd";
+import "./style.less";
 const { RangePicker } = DatePicker;
 const { Content } = Layout;
 interface Props {
   props: HighchartsReact.Props;
-  errorPictureStore: any
+  errorPictureStore: any;
 }
-
 
 interface HighchartsOption extends Highcharts.Options {
   legend?: any;
-  yAxis: any
+  yAxis: any;
 }
 
-@inject('errorPictureStore') @observer
+@inject("errorPictureStore")
+@observer
 class CustomList extends React.Component<Props, any> {
-
   state = {
     options: [],
-    noData: false
+    noData: false,
+    type: "day"
   };
 
-
   async componentDidMount() {
-    const { errorPictureStore } = this.props
-    const { siteId } = errorPictureStore
-    const result = await axios.get(`/api/metric/${siteId}?type=2`)
-    const data = result.data.data.data
-    const options = this.formatOptions(data)
-    this.setState({ options })
+    const { errorPictureStore } = this.props;
+    const { siteId } = errorPictureStore;
+    const result = await axios.get(`/api/metric/${siteId}?type=2`);
+    const data = result.data.data.data;
+    const options = this.formatOptions(data);
+    this.setState({ options });
     if (data.length) {
-      const id = result.data.data.data[0].id + ''
-      errorPictureStore.setFactor([id])
-      this.onLoad([id])
+      const id = result.data.data.data[0].id + "";
+      errorPictureStore.setFactor([id]);
+      this.onLoad([id]);
     }
   }
 
   formatOptions(data) {
-    let options: any = []
+    let options: any = [];
     for (let i = 0; i < data.length; i++) {
       const item = {
-        value: data[i].id + '',
+        value: data[i].id + "",
         label: data[i].display_name,
         isLeaf: false
-      }
-      options.push(item)
+      };
+      options.push(item);
     }
-    return options
+    return options;
   }
 
   formatData(charts) {
     const series = charts.series.map(item => {
-      item['type'] = 'line'
-      return item
-    })
+      item["type"] = "line";
+      return item;
+    });
 
     return {
-
       title: {
-        text: '错误图表'
+        text: "错误图表"
       },
       credits: {
         enabled: false
@@ -76,87 +74,86 @@ class CustomList extends React.Component<Props, any> {
       },
       xAxis: {
         categories: charts.categories,
-        tickInterval: 6,
+        tickInterval: 6
       },
       yAxis: {
         title: {
-          align: 'high',
-          text: '单位：',
+          align: "high",
+          text: "单位：",
           offset: -20,
           rotation: 0,
           y: -20
         },
-        lineWidth: 1,
+        lineWidth: 1
       },
       legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle"
       },
       series
-
-    }
+    };
   }
 
-  onLoad = (arr) => {
-    const { errorPictureStore } = this.props
-    errorPictureStore.setFactor(arr)
-    errorPictureStore.getCharts(arr)
-    errorPictureStore.getSameRingRatio()
-  }
+  onLoad = arr => {
+    const { errorPictureStore } = this.props;
+    errorPictureStore.setFactor(arr);
+    errorPictureStore.getCharts(arr);
+    errorPictureStore.getSameRingRatio();
+  };
 
-  onChange = (arr) => {
-    this.onLoad(arr)
-  }
+  onChange = arr => {
+    this.onLoad(arr);
+  };
 
-  formatResult = (data) => {
+  formatResult = data => {
     return data.map(d => {
       return {
         value: d,
         label: d,
         isLeaf: false
-      }
-    })
-  }
+      };
+    });
+  };
 
   loadData = async selectedOptions => {
-    const { errorPictureStore } = this.props
-    const { siteId, metricId, factor } = errorPictureStore
+    const { errorPictureStore } = this.props;
+    const { siteId, metricId, factor } = errorPictureStore;
     const targetOption = selectedOptions[selectedOptions.length - 1];
     targetOption.loading = true;
 
-    const params = factor.slice(1).join(',')
-    const query = `?site_id=${siteId}&metric_id=${metricId}&metrics=${params}`
-    const result = await axios.get(`/api/metric/factors${query}`)
-    const { data } = result.data
-    const children = this.formatResult(data)
+    const params = factor.slice(1).join(",");
+    const query = `?site_id=${siteId}&metric_id=${metricId}&metrics=${params}`;
+    const result = await axios.get(`/api/metric/factors${query}`);
+    const { data } = result.data;
+    const children = this.formatResult(data);
     targetOption.children = data[0] ? children : [];
 
     targetOption.loading = false;
     this.setState({
       options: [...this.state.options]
-    })
-  }
+    });
+  };
 
-  dataChange = ({ }, data) => {
-    const { errorPictureStore } = this.props
-    const { factor } = errorPictureStore
+  dataChange = ({}, data) => {
+    const { errorPictureStore } = this.props;
+    const { factor } = errorPictureStore;
     errorPictureStore.setCriteria({
       start_time: data[0],
       end_time: data[1]
-    })
-    errorPictureStore.getCharts(factor)
-  }
+    });
+    errorPictureStore.getCharts(factor);
+  };
 
   formatRatio(ratios) {
     const series = ratios.series.map(item => {
-      item['type'] = 'line'
-      return item
-    })
+      item["type"] = "line";
+      return item;
+    });
 
     return {
       title: {
-        text: '同环比图'
+        text: "同环比图"
       },
       credits: {
         enabled: false
@@ -166,39 +163,51 @@ class CustomList extends React.Component<Props, any> {
       },
       xAxis: {
         categories: ratios.categories,
-        tickInterval: 6,
+        tickInterval: 6
       },
       yAxis: {
         title: {
-          align: 'high',
-          text: '单位：ms',
+          align: "high",
+          text: "单位：ms",
           offset: -20,
           rotation: 0,
           y: -20
         },
-        lineWidth: 1,
+        lineWidth: 1
       },
       legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle"
       },
       series
-
-    }
+    };
   }
 
+  periodChange = e => {
+    const { errorPictureStore } = this.props;
+    const { factor } = errorPictureStore;
+    errorPictureStore.setCriteria({
+      type: e.target.value
+    });
+    errorPictureStore.getCharts(factor);
+  };
+
   render() {
-    const { errorPictureStore } = this.props
-    const { factor, charts, criteria, sameRingRatio } = errorPictureStore
-    const highchartOptions: HighchartsOption = this.formatData(charts)
-    const sameRingRatioOption: HighchartsOption = this.formatRatio(sameRingRatio)
+    const { errorPictureStore } = this.props;
+    const { factor, charts, criteria, sameRingRatio } = errorPictureStore;
+    const highchartOptions: HighchartsOption = this.formatData(charts);
+    const sameRingRatioOption: HighchartsOption = this.formatRatio(
+      sameRingRatio
+    );
     if (this.state.noData) {
       return (
-        <div style={{ margin: '200px auto', textAlign: 'center' }}>暂无数据</div>
-      )
+        <div style={{ margin: "200px auto", textAlign: "center" }}>
+          暂无数据
+        </div>
+      );
     }
-    if (!factor.length) return null
+    if (!factor.length) return null;
     return (
       <Content>
         <div className="dashboard-select">
@@ -212,9 +221,18 @@ class CustomList extends React.Component<Props, any> {
                 changeOnSelect
               />
             </Fieldset.Row>
+            <Fieldset.Row label="周期：">
+              <Radio.Group onChange={this.periodChange} value={criteria.type}>
+                <Radio value="mins">分钟</Radio>
+                <Radio value="day">天</Radio>
+              </Radio.Group>
+            </Fieldset.Row>
             <Fieldset.Row label="时间：">
               <RangePicker
-                defaultValue={[moment(criteria.start_time, 'YYYY-MM-DD HH:mm'), moment(criteria.end_time, 'YYYY-MM-DD HH:mm')]}
+                defaultValue={[
+                  moment(criteria.start_time, "YYYY-MM-DD HH:mm"),
+                  moment(criteria.end_time, "YYYY-MM-DD HH:mm")
+                ]}
                 onChange={this.dataChange}
                 format="YYYY-MM-DD HH:mm"
               />
@@ -236,11 +254,9 @@ class CustomList extends React.Component<Props, any> {
             {...this.props}
           />
         </div>
-
-
       </Content>
-    )
+    );
   }
 }
 
-export default CustomList
+export default CustomList;
