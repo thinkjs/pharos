631e4d463ff76f0ecaf4d194f3f6fe6c732bcf7b
linxd <linxd@touzhijia.com>
2019-08-05 17:08:21 +0800

    添加perf

--- www/src/pages/Monitor/Perf/index.tsx
+++ www/src/pages/Monitor/Perf/index.tsx
@@ -1,77 +1,74 @@
-import * as React from 'react';
-import { Layout, Cascader } from 'antd'
-import { observer, inject } from 'mobx-react';
+import * as React from "react";
+import { Layout, Cascader } from "antd";
+import { observer, inject } from "mobx-react";
 import * as Highcharts from "highcharts";
-import HighchartsReact from 'highcharts-react-official'
-import axios from '@utils/axios'
-import Fieldset from '@components/Fieldset'
-import moment from '@utils/moment'
-import { DatePicker } from 'antd';
-import './style.less'
+import HighchartsReact from "highcharts-react-official";
+import axios from "@utils/axios";
+import Fieldset from "@components/Fieldset";
+import moment from "@utils/moment";
+import { DatePicker, Radio } from "antd";
+import "./style.less";
 const { RangePicker } = DatePicker;
 const { Content } = Layout;
 interface Props {
   props: HighchartsReact.Props;
-  perfStore: any
+  perfStore: any;
 }
-
 
 interface HighchartsOption extends Highcharts.Options {
   legend?: any;
-  yAxis: any
+  yAxis: any;
 }
 
-@inject('perfStore') @observer
+@inject("perfStore")
+@observer
 class Perf extends React.Component<Props, any> {
-
   state = {
     options: [],
     noData: false
   };
 
-
   async componentDidMount() {
-    const { perfStore } = this.props
-    const { siteId } = perfStore
-    const result = await axios.get(`/api/metric/${siteId}?type=1`)
-    const data = result.data.data.data
+    const { perfStore } = this.props;
+    const { siteId } = perfStore;
+    const result = await axios.get(`/api/metric/${siteId}?type=1`);
+    const data = result.data.data.data;
 
-    const options = this.formatOptions(data)
-    this.setState({ options })
+    const options = this.formatOptions(data);
+    this.setState({ options });
     if (data.length) {
-      const id = data[0].id + ''
-      perfStore.setFactor([id])
-      this.onLoad([id])
+      const id = data[0].id + "";
+      perfStore.setFactor([id]);
+      this.onLoad([id]);
     } else {
       this.setState({
         noData: true
-      })
+      });
     }
   }
 
   formatOptions(data) {
-    let options: any = []
+    let options: any = [];
     for (let i = 0; i < data.length; i++) {
       const item = {
-        value: data[i].id + '',
+        value: data[i].id + "",
         label: data[i].display_name,
         isLeaf: false
-      }
-      options.push(item)
+      };
+      options.push(item);
     }
-    return options
+    return options;
   }
 
   formatData(charts) {
     const series = charts.series.map(item => {
-      item['type'] = 'line'
-      return item
-    })
+      item["type"] = "line";
+      return item;
+    });
 
     return {
-
       title: {
-        text: '性能图表'
+        text: "性能图表"
       },
       credits: {
         enabled: false
@@ -81,87 +78,86 @@ class Perf extends React.Component<Props, any> {
       },
       xAxis: {
         categories: charts.categories,
-        tickInterval: 6,
+        tickInterval: 6
       },
       yAxis: {
         title: {
-          align: 'high',
-          text: '单位：',
+          align: "high",
+          text: "单位：",
           offset: -20,
           rotation: 0,
           y: -20
         },
-        lineWidth: 1,
+        lineWidth: 1
       },
       legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle"
       },
       series
-
-    }
+    };
   }
 
-  onLoad = (arr) => {
-    const { perfStore } = this.props
-    perfStore.setFactor(arr)
-    perfStore.getCharts(arr)
-    perfStore.getSameRingRatio()
-  }
+  onLoad = arr => {
+    const { perfStore } = this.props;
+    perfStore.setFactor(arr);
+    perfStore.getCharts(arr);
+    perfStore.getSameRingRatio();
+  };
 
-  onChange = (arr) => {
-    this.onLoad(arr)
-  }
+  onChange = arr => {
+    this.onLoad(arr);
+  };
 
-  formatResult = (data) => {
+  formatResult = data => {
     return data.map(d => {
       return {
         value: d,
         label: d,
         isLeaf: false
-      }
-    })
-  }
+      };
+    });
+  };
 
   loadData = async selectedOptions => {
-    const { perfStore } = this.props
-    const { siteId, metricId, factor } = perfStore
+    const { perfStore } = this.props;
+    const { siteId, metricId, factor } = perfStore;
     const targetOption = selectedOptions[selectedOptions.length - 1];
     targetOption.loading = true;
 
-    const params = factor.slice(1).join(',')
-    const query = `?site_id=${siteId}&metric_id=${metricId}&metrics=${params}`
-    const result = await axios.get(`/api/metric/factors${query}`)
-    const { data } = result.data
-    const children = this.formatResult(data)
+    const params = factor.slice(1).join(",");
+    const query = `?site_id=${siteId}&metric_id=${metricId}&metrics=${params}`;
+    const result = await axios.get(`/api/metric/factors${query}`);
+    const { data } = result.data;
+    const children = this.formatResult(data);
     targetOption.children = data[0] ? children : [];
 
     targetOption.loading = false;
     this.setState({
       options: [...this.state.options]
-    })
-  }
+    });
+  };
 
-  dataChange = ({ }, data) => {
-    const { perfStore } = this.props
-    const { factor } = perfStore
+  dataChange = ({}, data) => {
+    const { perfStore } = this.props;
+    const { factor } = perfStore;
     perfStore.setCriteria({
       start_time: data[0],
       end_time: data[1]
-    })
-    perfStore.getCharts(factor)
-  }
+    });
+    perfStore.getCharts(factor);
+  };
 
   formatRatio(ratios) {
     const series = ratios.series.map(item => {
-      item['type'] = 'line'
-      return item
-    })
+      item["type"] = "line";
+      return item;
+    });
 
     return {
       title: {
-        text: '同环比图'
+        text: "同环比图"
       },
       credits: {
         enabled: false
@@ -171,39 +167,51 @@ class Perf extends React.Component<Props, any> {
       },
       xAxis: {
         categories: ratios.categories,
-        tickInterval: 6,
+        tickInterval: 6
       },
       yAxis: {
         title: {
-          align: 'high',
-          text: '单位：ms',
+          align: "high",
+          text: "单位：ms",
           offset: -20,
           rotation: 0,
           y: -20
         },
-        lineWidth: 1,
+        lineWidth: 1
       },
       legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle"
       },
       series
-
-    }
+    };
   }
 
+  periodChange = e => {
+    const { perfStore } = this.props;
+    const { factor } = perfStore;
+    perfStore.setCriteria({
+      type: e.target.value
+    });
+    perfStore.getCharts(factor);
+  };
+
   render() {
-    const { perfStore } = this.props
-    const { factor, charts, criteria, sameRingRatio } = perfStore
-    const highchartOptions: HighchartsOption = this.formatData(charts)
-    const sameRingRatioOption: HighchartsOption = this.formatRatio(sameRingRatio)
+    const { perfStore } = this.props;
+    const { factor, charts, criteria, sameRingRatio } = perfStore;
+    const highchartOptions: HighchartsOption = this.formatData(charts);
+    const sameRingRatioOption: HighchartsOption = this.formatRatio(
+      sameRingRatio
+    );
     if (this.state.noData) {
       return (
-        <div style={{ margin: '200px auto', textAlign: 'center' }}>暂无数据</div>
-      )
+        <div style={{ margin: "200px auto", textAlign: "center" }}>
+          暂无数据
+        </div>
+      );
     }
-    if (!factor.length) return null
+    if (!factor.length) return null;
     return (
       <Content>
         <div className="dashboard-select">
@@ -217,9 +225,18 @@ class Perf extends React.Component<Props, any> {
                 changeOnSelect
               />
             </Fieldset.Row>
+            <Fieldset.Row label="周期：">
+              <Radio.Group onChange={this.periodChange} value={criteria.type}>
+                <Radio value="mins">分钟</Radio>
+                <Radio value="day">天</Radio>
+              </Radio.Group>
+            </Fieldset.Row>
             <Fieldset.Row label="时间：">
               <RangePicker
-                defaultValue={[moment(criteria.start_time, 'YYYY-MM-DD HH:mm'), moment(criteria.end_time, 'YYYY-MM-DD HH:mm')]}
+                defaultValue={[
+                  moment(criteria.start_time, "YYYY-MM-DD HH:mm"),
+                  moment(criteria.end_time, "YYYY-MM-DD HH:mm")
+                ]}
                 onChange={this.dataChange}
                 format="YYYY-MM-DD HH:mm"
               />
@@ -241,11 +258,9 @@ class Perf extends React.Component<Props, any> {
             {...this.props}
           />
         </div>
-
-
       </Content>
-    )
+    );
   }
 }
 
-export default Perf
+export default Perf;
--- www/src/pages/Monitor/store/perf.ts
+++ www/src/pages/Monitor/store/perf.ts
@@ -1,99 +1,94 @@
-import { observable, action, computed } from 'mobx';
-import axios from '@utils/axios';
-import moment from 'moment'
+import { observable, action, computed } from "mobx";
+import axios from "@utils/axios";
+import moment from "moment";
 
 class CustomStore {
-
-
   @computed get siteId() {
-    return localStorage.getItem('projectId')
+    return localStorage.getItem("projectId");
   }
 
   @computed get timeInterval() {
     const start = moment(this.criteria.start_time).unix();
     const end = moment(this.criteria.end_time).unix();
-    return (end - start) / 60 / 60 > 24
+    return (end - start) / 60 / 60 > 24;
   }
 
   initData = () => {
     return {
-      start_time: moment().subtract(1, 'days'),
-      end_time: moment(),
-      metrics: '',
-      type: 'mins'
-    }
-  }
+      start_time: moment()
+        .subtract(1, "days")
+        .format("YYYY-MM-DD HH:mm"),
+      end_time: moment().format("YYYY-MM-DD HH:mm"),
+      metrics: "",
+      type: "mins"
+    };
+  };
 
   @observable criteria = this.initData();
 
-  @action setCriteria = (data) => {
+  @action setCriteria = data => {
     if (data) {
-      this.criteria = Object.assign({}, this.criteria, data)
+      this.criteria = Object.assign({}, this.criteria, data);
     } else {
-      this.criteria = this.initData()
+      this.criteria = this.initData();
     }
-  }
-
+  };
 
   @observable charts = {
     categories: [],
     series: []
-  }
-  @action setCharts = (values) => this.charts = values
+  };
+  @action setCharts = values => (this.charts = values);
 
+  @observable list = [];
+  @action setList = list => (this.list = list);
 
-  @observable list = []
-  @action setList = (list) => this.list = list
-
-
-  @observable options = []
-  @action setOptions = (data) => this.options = data
-  @observable.ref factor = []
-  @action setFactor = (val) => this.factor = val
+  @observable options = [];
+  @action setOptions = data => (this.options = data);
+  @observable.ref factor = [];
+  @action setFactor = val => (this.factor = val);
 
   @computed get metricId() {
-    return this.factor[0]
+    return this.factor[0];
   }
 
-  @observable metricList = []
-  @action setMetricList = (list) => this.metricList = list
+  @observable metricList = [];
+  @action setMetricList = list => (this.metricList = list);
 
-
-
-
-  formatCriteria = (factor) => {
-    const metrics = factor.slice(1).join(',')
-    const criteria = Object.assign(this.criteria, { metrics })
-    let query = ''
+  formatCriteria = factor => {
+    const metrics = factor.slice(1).join(",");
+    const criteria = Object.assign({}, this.criteria, { metrics });
+    let query = "";
     if (this.timeInterval) {
-      criteria.type = 'day'
+      criteria.type = "day";
     }
     Object.keys(criteria).map(key => {
       if (criteria[key]) {
-        query += `&${key}=${criteria[key]}`
+        query += `&${key}=${criteria[key]}`;
       }
-    })
-    return query
-  }
+    });
+    return query;
+  };
 
-  @action getCharts = async (factor) => {
-    const params = this.formatCriteria(factor)
-    const query = `?site_id=${this.siteId}&metric_id=${this.metricId}${params}`
-    const result = await axios.get(`/api/metric/custom_time${query}`)
-    this.setCharts(result.data.data)
-  }
+  @action getCharts = async factor => {
+    const params = this.formatCriteria(factor);
+    const query = `?site_id=${this.siteId}&metric_id=${this.metricId}${params}`;
+    const result = await axios.get(`/api/metric/custom_time${query}`);
+    this.setCharts(result.data.data);
+  };
 
   @observable.ref sameRingRatio = {
     series: [],
     categories: []
-  }
-  @action setSameRingRatio = (data) => this.sameRingRatio = data
+  };
+  @action setSameRingRatio = data => (this.sameRingRatio = data);
   @action getSameRingRatio = async () => {
-    const result = await axios.get(`/api/metric/ratio?site_id=${this.siteId}&metric_id=${this.metricId}`);
+    const result = await axios.get(
+      `/api/metric/ratio?site_id=${this.siteId}&metric_id=${this.metricId}`
+    );
 
-    this.setSameRingRatio(result.data.data)
-  }
-
+    this.setSameRingRatio(result.data.data);
+  };
 }
 
-export default new CustomStore()
+export default new CustomStore();
